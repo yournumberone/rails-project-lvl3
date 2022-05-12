5f214976ba622cbcc3d8d77f0affcab537ad93c6
yournumberone <marsthegod@protonmail.com>
2022-05-12 22:49:39 +0300

    import scss to js

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "@fortawesome/fontawesome-free/css/all"
+import "styles/application.scss"
 require("bootstrap");
 const images = require.context('../images', true)
 
