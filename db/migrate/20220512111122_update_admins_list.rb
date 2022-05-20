315f2b354f1b945dd52b408286717e6ab711f4fa
yournumberone <marsthegod@protonmail.com>
2022-05-20 11:47:56 +0300

    upd migration

--- db/migrate/20220512111122_update_admins_list.rb
+++ db/migrate/20220512111122_update_admins_list.rb
@@ -1,5 +1,5 @@
 class UpdateAdminsList < ActiveRecord::Migration[6.1]
   def change
-    User.find_by(email: 'mars.shamgulov@gmail.com').update(admin: true)
+    User.find_by(email: 'mars.shamgulov@gmail.com').try(:update, { admin: true })
   end
 end
